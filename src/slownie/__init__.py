0214dbd026810aab923ea5d70acc4a07f945a818
Adam Karpierz <akarpierz@gmail.com>
2026-01-26 16:24:56 +0100

    Update documentation

--- src/slownie/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2016 Adam Karpierz
-# SPDX-License-Identifier: Zlib
-
-from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
-
-from ._slownie import * ; del _slownie  # type: ignore[name-defined]  # noqa
